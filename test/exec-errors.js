9cfb6c95c3ba4939446c0177bdf316686da89e27
Alex Jordan <alex@strugee.net>
2016-11-13 20:11:30 -0800

    Test the handling of exec error handling

---
+++ test/exec-errors.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var assert = require('assert'),
+    vows = require('vows'),
+    childProcessTest = require('./lib/childprocess-object.js');
+
+var suite = vows.describe('Process exec error handling');
+
+suite.addBatch({
+	'When we require the module': {
+		topic: function() {
+			return require('../index.js');
+		},
+		'it works': function(spawn) {
+			assert.isFunction(spawn);
+		},
+		'and try to run a binary that doesn\'t exist': {
+			topic: function(spawn) {
+				var callback = this.callback;
+				var childProcess = spawn('nonexistant', [], process.cwd(), function(err, stdout) {
+					callback(childProcess, err, stdout);
+				});
+			},
+			'a `ChildProcess` object is returned': childProcessTest,
+			'an error is sent to the callback': function(childProcess, err, stdout) {
+				assert(err);
+				assert.equal(err.toString(), 'Error: spawn nonexistant ENOENT');
+			},
+			'the result is the null string': function(childProcess, err, stdout) {
+				assert.equal(stdout, '');
+			}
+		}
+	}
+});
+
+suite['export'](module);
