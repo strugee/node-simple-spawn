3c8e85ab14546414c47f6226db9f971748a50980
Alex Jordan <alex@strugee.net>
2016-11-13 18:13:53 -0800

    Expand the complex failure process test

--- test/complex-process-failure.js
+++ test/complex-process-failure.js
@@ -45,6 +45,22 @@ suite.addBatch({
 			'the result is a single newline': function(childProcess, err, stdout) {
 				assert.equal(stdout, '\n');
 			}
+		},
+		'and spawn a process that will fail and output to both stdout and stderr': {
+			topic: function(spawn) {
+				var callback = this.callback;
+				var childProcess = spawn('test/lib/test-output.js', ['1', 'foo', 'bar'], process.cwd(), function(err, stdout) {
+					callback(childProcess, err, stdout);
+				});
+			},
+			'a `ChildProcess` object is returned': childProcessTest,
+			'an error is sent to the callback': function(childProcess, err, stdout) {
+				assert(err);
+				assert.equal(err.toString(), 'Error: Process `test/lib/test-output.js 1 foo bar` exited with non-zero exit code 1; stderr is:\nbar\n');
+			},
+			'the result is `foo`': function(childProcess, err, stdout) {
+				assert.equal(stdout, 'foo\n');
+			}
 		}
 	}
 });
